f379f71b522d0d264919438995732d529f20d3b8
Swiwi2 <kwyoung93@gmail.com>
2024-08-06 12:38:39 +0200

    Map Edits & New City
    Added Serenleaf and Route 2. More edits to Route 1 and Woodholme.

--- data/event_scripts.s
+++ data/event_scripts.s
@@ -1098,3 +1098,7 @@ EventScript_VsSeekerChargingDone::
 	.include "data/maps/Woodholme_CondoLeft_Apt3/scripts.inc"
 
 	.include "data/maps/Woodholme_CondoLeft_PlayerHouse/scripts.inc"
+
+	.include "data/maps/SerenleafCity/scripts.inc"
+
+	.include "data/maps/Route2/scripts.inc"
